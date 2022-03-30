87e80af0007de374a64def0d7044b69b2454283b
HaloNee <49353899+HaloNee@users.noreply.github.com>
2022-03-30 13:58:08 +0800

    update

---
+++ code/getDataFromJQ.py
@@ -0,0 +1,16 @@
+# import datetime
+# import numpy as np
+# import pandas as pd
+# import pathlib
+from jqdatasdk import *
+
+auth('18210006280', 'Programming6280')
+
+data_dir = '../data/'
+
+# data_path = pathlib.Path.cwd() / ".." / "data" / "CSI300.csv"
+
+df = get_price('000300.XSHG', start_date='2010-01-01',
+               end_date='2021-12-31', frequency='daily', panel=False)
+print(df.head())
+df.to_csv(data_dir + 'CSI300.csv')
