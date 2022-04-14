316dfbdd816aae49967bb5c732ee88b6362ec982
algo21-221040092 <101452065+algo21-221040092@users.noreply.github.com>
2022-04-14 16:29:12 +0800

    Update Backtrader基础框架.py
    可以回测了，回测结果包含指标数值和图，指标可以在py里面看到，图只能在notebook里看。
    在之前的csv加入预测处理好的当天信号放到df里即可回测

--- code/Backtrader基础框架.py
+++ code/Backtrader基础框架.py
@@ -1,32 +1,25 @@
 import pandas as pd
 import datetime as dt
 import backtrader as bt
-import pyfolio as pf
+import quantstats as qs
 import matplotlib as plt
 import numpy as np
 import os
 
-import pathlib
-
-# import warnings
-# warnings.filterwarnings("ignore")
-
 os.getcwd()
 
-#数据加载
-
-df_data_path = pathlib.Path.cwd() / ".." / "data" / "CSI300.csv"
-
-df=pd.read_csv(df_data_path)
+#数据加载-------------------------------------
+df=pd.read_csv(r'C:\Users\31145\PycharmProjects\Backtrader学习\CSI300.csv')#绝对路径
 df.index=pd.to_datetime(df.date)
 df['openinterest']=0
 
+#以下是自己随机添加signal的之后的文件，hmm算法给出的带signal的文件直接用上面的df=pd.read_csv导入，把下面到构建策略之前mute掉即可
 
-#随机生成一个-1，0，1的predictions序列
+#随机生成一个-1，0，1的predictions序列---------
 sig=[]
 for i in range(len(df)):
     x=np.random.randn()
-    if x> 0.2:
+    if x > 0.2:
         sig.append(1)
     elif x<-0.2:
         sig.append(-1)
@@ -36,14 +29,13 @@ df['signal']=sig
 
 
 
-#构建策略-------------------------------
+#构建策略-------------------------------------
 class TestSignalStrategy(bt.Strategy):
-    params=(
-    )
+    params=()
 
-    def log(self,txt):
-        dt= self.datas[0].datetime.date(0)
-        print('{},{}'.format(dt.isoformat(),txt)) 
+    def log(self,txt,dt=None):
+        dt= dt or self.datas[0].datetime.date(0)
+        print('%s,%s' % (dt.isoformat(),txt)) 
 
     def __init__(self):
         self.bar_num=0
@@ -54,9 +46,7 @@ class TestSignalStrategy(bt.Strategy):
     def prenext(self):
         self.next()
 
-    
     def next(self):
-        #假设有100万资金，每次成分股调整，每个股票使用1万元
         self.bar_num+=1
         current_date=self.datas[0].datetime.date(0).strftime("%Y-%m-%d")
         try:
@@ -69,90 +59,94 @@ class TestSignalStrategy(bt.Strategy):
                 self.log("下个交易日的信号不存在")
             else:
                 signal = int(next_signal_df['signal'])
-                close_price = float(next_signal_df['close'])
                 
                 #第一次交易没有底仓，只允许做多
                 #交易信号的执行方式：
-                #signal为1，以开盘价买入45%的资金，然后以收盘价卖出昨日全部底仓
-                #signal为-1时，以开盘价卖出45%的资金，以收盘价买入平仓。
+                #先不考虑卖空
+                #signal为1，以当天开盘价全仓买入
+                #signal为-1时，以当天开盘价卖出所有持仓
                 if self.first_trade:
                     if signal==-1:
                         pass
                     if signal == 1:
                         total_value = self.broker.get_value()
-                        next_open_price = self.datas[0].open[1]
-                        target_size=(0.01*0.45*total_value/next_open_price)*100
-                        self.buy(self.datas[0],size=target_size)
+                        open_price = self.datas[0].open[0]
+                        target_size=total_value//open_price
+                        self.buy(self.datas[0],size=target_size,price=open_price)
                         self.first_trade=False
                 else:
                     #现有持仓
                     now_hold_size = self.getposition(self.datas[0]).size
                     if signal == 1:
                         total_value = self.broker.get_value()
-                        next_open_price=self.datas[0].open[1]
-                        target_size=(0.01*0.45*total_value/next_open_price)*100
-                        self.buy(self.datas[0],size=target_size)
-                        self.sell(self.datas[0],size=now_hold_size,price=close_price)
+                        open_price = self.datas[0].open[0]
+                        target_size=total_value//open_price
+                        self.buy(self.datas[0],size=target_size, price=open_price)
+
                     if signal == -1:
                         total_value = self.broker.get_value()
-                        next_open_price =self.datas[0].open[1]
-                        target_size=(0.01*0.45*total_value/next_open_price)*100
+                        open_price = self.datas[0].open[0]
+                        target_size=total_value//open_price
                         #卖出开仓，手数不能超过底仓
                         if target_size>now_hold_size:
                             target_size=now_hold_size
 
-                        self.sell(self.datas[0],size=target_size)
-                        self.buy(self.datas[0],size=now_hold_size,price=close_price)
+                        self.sell(self.datas[0],size=target_size, price=open_price)
+
     #订单情况
     def notify_order(self, order):
         
+        #订单处于未决，继续
         if order.status in [order.Submitted, order.Accepted]:
             return
         
-        if order.status == order.Rejected:
-            self.log(f"Rejected : order_ref:{order.ref}  data_name:{order.p.data._name}")
-            
-        if order.status == order.Margin:
-            self.log(f"Margin : order_ref:{order.ref}  data_name:{order.p.data._name}")
-            
-        if order.status == order.Cancelled:
-            self.log(f"Concelled : order_ref:{order.ref}  data_name:{order.p.data._name}")
-            
-        if order.status == order.Partial:
-            self.log(f"Partial : order_ref:{order.ref}  data_name:{order.p.data._name}")
-         
-        if order.status == order.Completed:
+        #订单已决
+        if order.status in [order.Completed]:
             if order.isbuy():
-                self.log(f" BUY : data_name:{order.p.data._name} price : {order.executed.price} , cost : {order.executed.value} , commission : {order.executed.comm}")
+                self.log('买单执行,%.2f' % order.executed.price)
 
-            else:  # Sell
-                self.log(f" SELL : data_name:{order.p.data._name} price : {order.executed.price} , cost : {order.executed.value} , commission : {order.executed.comm}")
-    
-    def notify_trade(self, trade):
-        # 一个trade结束的时候输出信息
-        if trade.isclosed:
-            self.log('closed symbol is : {} , total_profit : {} , net_profit : {}' .format(
-                            trade.getdataname(),trade.pnl, trade.pnlcomm))
-            # self.trade_list.append([self.datas[0].datetime.date(0),trade.getdataname(),trade.pnl,trade.pnlcomm])
-            
-        if trade.isopen:
-            self.log('open symbol is : {} , price : {} ' .format(
-                            trade.getdataname(),trade.price))
-    def stop(self):
+            elif order.issell():
+                self.log('卖单执行,%2f' % order.executed.price)
         
-        pass 
+        elif order.status in [order.Canceled,order.Rejected,order.Expired]:
+            self.log('订单 Canceled/Rejected/Expired')
+
+    #交易情况
+    def notify_trade(self, trade):
+        if trade.isclosed:
+            print('毛收益 %0.2f, 扣佣后收益 % 0.2f , 佣金 %.2f'
+            % (trade.pnl, trade.pnlcomm, trade.commission))
+        
+
 
 cerebro=bt.Cerebro()
 
 stock_df=df[['open','high','low','close','volume','openinterest']]
 data=bt.feeds.PandasData(dataname=stock_df,fromdate=dt.datetime(2010,1,4),todate=dt.datetime(2021,12,31),timeframe=bt.TimeFrame.Days)
 cerebro.adddata(data)
-cerebro.broker.setcash(1000000.0)
+
+#初始资金设置为10万
+cerebro.broker.setcash(100000.0)
+#手续费万分之二
 cerebro.broker.setcommission(commission=0.0002)
+#滑点设置 0.5%
+cerebro.broker = bt.brokers.BackBroker(slip_perc=0.005)
 
 cerebro.addstrategy(TestSignalStrategy)
-cerebro.addanalyzer(bt.analyzers.PyFolio)
+cerebro.addanalyzer(bt.analyzers.PyFolio,_name='PyFolio')
 
 #运行回测
-results= cerebro.run()
-cerebro.plot(style='candle')
+print('start portfolio value:',format(cerebro.broker.getvalue()))
+results = cerebro.run()
+print('end portfolio value:',format(cerebro.broker.getvalue()))
+
+
+#绩效评价----------------------------
+#获取策略实例
+start = results[0]
+portfolio_stats = start.analyzers.getbyname('PyFolio')
+#以下returns为以日期为索引的资产日收益率序列
+returns,positions,transactions,gross_lev = portfolio_stats.get_pf_items()
+returns.index = returns.index.tz_convert(None)
+#画图仍只能在notebook中输出
+qs.reports.basic(returns,benchmark=None,rf=0.0,grayscale=False,display=True,compounded = True)
